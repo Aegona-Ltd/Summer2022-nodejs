bbb2ea7c592f83829a403bed7d9f1252890192ee
Trần Quốc Hiếu <hieutran160120@gmail.com>
2022-10-04 10:30:49 +0700

    Code first

--- [TheLuc]Shopping_Cart/src/util/mongoose.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  multipleMongooseToObject: function (mongooses) {
-    return mongooses.map((mongooses) => mongooses.toObject());
-  },
-  mongooseToObject: function (mongooses) {
-    return mongooses ? mongooses.toObject() : mongooses;
-  },
-};
